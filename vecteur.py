af30160d604583df2d0d0d885473fa14681ef398
Martino Yovo <martinosoyen@gmail.com>
2021-12-09 21:13:33 +0100

    Adding vectorsum method

--- vecteur.py
+++ vecteur.py
@@ -8,7 +8,11 @@ class Vecteur:
     def norme(self):
         n = sqrt(self.x**2+self.y**2)
         return n
+    def sum(self, t):
+        self.x+=t.x
+        self.y+=t.y
 
+v1 = Vecteur(x=3, y=0)
+v2 = Vecteur(x=6, y=3)
 
-v = Vecteur(x=3, y=9)
-print(v.norme())
+v1.sum(v2)
