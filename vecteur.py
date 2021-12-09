cd955cd73ead7fb5dd5d1d84e58352766e423d2f
Martino Yovo <martinosoyen@gmail.com>
2021-12-09 21:31:53 +0100

    Sub method added

--- vecteur.py
+++ vecteur.py
@@ -1,18 +1,23 @@
 from math import sqrt
 
-class Vecteur:
+class Vector:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def norme(self):
+    def norm(self):
         n = sqrt(self.x**2+self.y**2)
         return n
     def sum(self, t):
         self.x+=t.x
         self.y+=t.y
 
-v1 = Vecteur(x=3, y=0)
-v2 = Vecteur(x=6, y=3)
+    def sub(self, t):
+        self.x-=t.x
+        self.y-=t.y
+
+v1 = Vector(x=3, y=0)
+v2 = Vector(x=6, y=3)
 
 v1.sum(v2)
+v1.sub(v2)
