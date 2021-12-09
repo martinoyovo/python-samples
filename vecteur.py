5cbcf5d8b2db3a700569679498e8ac89fce0fad6
Martino Yovo <martinosoyen@gmail.com>
2021-12-09 20:30:30 +0100

    Defined a method to get a norm of a vecteur

--- vecteur.py
+++ vecteur.py
@@ -1,4 +1,14 @@
+from math import sqrt
+
 class Vecteur:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def norme(self):
+        n = sqrt(self.x**2+self.y**2)
+        return n
+
+
+v = Vecteur(x=3, y=9)
+print(v.norme())
