afc6887718ade5793cef3294c5951eb52eb6ff8a
Martino Yovo <martinosoyen@gmail.com>
2021-12-16 20:42:57 +0100

    __repr__ and __str__ magic method added

--- vecteur.py
+++ vecteur.py
@@ -1,9 +1,22 @@
 from math import sqrt
 
 class Vector:
-    def __init__(self, x, y):
+
+    #initialization
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    #magic method to customize object representation
+    def __repr__(self):
+        # here you can what you should do if you want to create the object
+        # use this when you want to write something for another developer that will use your code
+        return f'Vector1{self.x}, {self.y}'
+
+    # magic method to customize what is returned by the print function
+    def __str__(self):
+        # this is like a print method
+        return f'This is to print that Vector1=({self.x}, {self.y})'
 
     def norm(self):
         n = sqrt(self.x**2+self.y**2)
